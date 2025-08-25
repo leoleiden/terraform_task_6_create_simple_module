54053d400417352661a0e557ac4a1c35b4eee8e3
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-08-25 16:46:41 +0300

    Update variables.tf

--- modules/resource_group_storage/variables.tf
+++ modules/resource_group_storage/variables.tf
@@ -12,3 +12,15 @@ variable "storage_account_name" {
   description = "The name of the storage account"
   type        = string
 }
+
+variable "storage_account_tier" {
+  description = "The storage account tier"
+  type        = string
+  default     = "Standard"
+}
+
+variable "storage_account_replication_type" {
+  description = "The storage account replication type"
+  type        = string
+  default     = "GRS"
+}
