6d99d3663259b10705c34cc449f521c88594044d
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-08-25 16:50:15 +0300

    Update outputs.tf

--- modules/resource_group_storage/outputs.tf
+++ modules/resource_group_storage/outputs.tf
@@ -7,3 +7,11 @@ output "storage_account_id" {
   value       = azurerm_storage_account.storage.id
   description = "The ID of the created storage account"
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.this.name
+}
